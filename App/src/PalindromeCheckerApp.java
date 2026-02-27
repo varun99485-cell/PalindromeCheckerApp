2051ba073234cd4fb1cc3fdead9d6c8a27eff444
varun99485-cell <varun99485@gmail.com>
2026-02-27 22:39:26 +0530

    WELCOME

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
-public class PalindromeCheckerApp {
+public class PalindromeCheckerApp{
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Version : 1.0");
+        System.out.println("System initialized successfully.");
+    }
 }

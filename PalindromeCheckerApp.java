9c2fd1b3f48af694c4d8544328896cfad9270369
varun99485-cell <varun99485@gmail.com>
2026-03-08 21:17:05 +0530

    Welcome to PalindromeCheckerApp

--- PalindromeCheckerApp.java
+++ PalindromeCheckerApp.java
@@ -1,7 +1,14 @@
-public class PalindromeCheckerApp{
-    public static void main(String[] args){
-        System.out.println("Welcome to the Palindrome Checker Management System");
-        System.out.println("Version : 1.0");
-        System.out.println("System initialized successfully.");
+public class PalindromeCheckerApp {
+    public static void main(String args[]){
+        String str="madam";
+        boolean ispalindrome=true;
+        for(int i=0; i<str.length()/2; i++){
+            if(str.charAt(i) != str.charAt(str.length() -1 -i)){
+                ispalindrome = false;
+                break;
+            }
+        }
+        System.out.println("Input text: "+str);
+        System.out.println("\n Is it a palindrome : True");
     }
 }
